fc456e3862a16b2d9847d44dafe1f80b7d0fbf71
jobayer58 <jobayerafnan770@gmail.com>
2025-08-15 03:40:21 +0600

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,11 @@
+import pic from '../../../public/jhankar sir.jpg'
+
+import Image from "next/image";
+
 export default function About() {
-    return <h1>about me</h1>
+    return (
+        <div>
+            <Image src={pic}  alt="logo" />
+        </div>
+    )
 }
